7bccd49ca1614a89378436b69e126e86183773ce
ImSanskar2 <91114238+ImSanskar2@users.noreply.github.com>
2021-09-22 10:49:52 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,42 @@
+print("Hello")
+a=46
+b=33
+a=b
+print (a)
+print(a-b)
+print(a+b)
+print(a/b)
+print(a*b)
+b=10
+print(a-b)
+print(a)
+print(a**b)
+print(a<b)
+print(a>b)
+print(a!=b)
+a= True
+print(a)
+b=False
+print(b)
+print(a&b)
+print(a|b)
+print(a|a)
+print(b|b)
+print ("Sanskar")
+str1 = "My name is Sanskar"
+print(str1)
+str2 = '''My name is Sanskar'''
+print(str2)
+first_string= "My Name Is Sanskar"
+print(first_string[0])
+print(len(first_string))
+print(first_string[1:-5])
+print(first_string[1:5])
+print(first_string[1:-5])
+print(first_string[-1:5])
+print(first_string.upper())
+print(first_string)
+print(first_string.lower())
+print(first_string)
+print(first_string.replace('Is','Are'))
+print(first_string)
